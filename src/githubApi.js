ba5db8f15c323971e725583af2e1f10f4b9f11e7
Marcelinne <marcela.montalvo@epn.educe>
2024-05-14 00:30:42 -0500

    migrate old repository

---
+++ src/githubApi.js
@@ -0,0 +1,65 @@
+// import { ValidationError , ConnectionError } from '../errors/errors';
+import dotenv from 'dotenv';
+
+import fs from 'fs';
+
+dotenv.config()
+const TOKEN = process.env.GITHUB_TOKEN;
+const BASEURL= 'https://api.github.com';
+const ORG_NAME = 'stackbuilders';
+const BASE_URL_ORG = `https://api.github.com/orgs/${ORG_NAME}/repos`;
+
+    export async function testConnection(token) {
+    if (!token) {
+        console.error('No se ha proporcionado un token de acceso. Configure la variable de entorno GITHUB_TOKEN.');
+        return false;
+    }
+
+    try {
+        const response = await fetch(`${BASEURL}/user`, {
+            headers: {
+                Authorization: `token ${token}`
+            }
+        });
+
+        if (response.ok) {
+            const userData = await response.json();
+            console.log('Conexión exitosa. Bienvenido, ' + userData.login);
+            return true;
+        } else {
+            console.error('Error de conexión:', response.statusText);
+            return false;
+        }
+    } catch (error) {
+        if (error instanceof ConnectionError) {
+            console.log("Connection Error:", error.message);
+        } else {
+            console.error('Error fetching repositories:', error.message);
+            throw error;
+        }
+        return false;
+    }
+}
+
+export async function getRepositories(authToken) {
+    try {
+        const response = await fetch(BASE_URL_ORG, {
+            headers: {
+                Authorization: `token ${authToken}`, 
+            },
+        });
+        const data = await response.json();
+        console.log(typeof data);
+        return data;
+    } catch (error) {
+        if (error instanceof ConnectionError) {
+            console.log("Connection Error:", error.message);
+        } else {
+            console.error('Error fetching repositories:', error.message);
+            throw error;
+        }
+        return null;
+    }
+}
+
+
---
+++ test/repositoryUtils.test.js
@@ -0,0 +1,110 @@
+const { filterRepositoriesWithMoreThanFiveStars } = require('../src/repositoryUtils.js');
+
+
+describe('filterRepositoriesWithMoreThanFiveStars', () => {
+    describe('when it receives an array of objects', () => {
+    test('Return repositories with more than 5 stars', () => {
+
+        const repositories = [
+        { id: 1, name: 'repo1', stargazers_count: 10 },
+        { id: 2, name: 'repo2', stargazers_count: 5 },
+        { id: 3, name: 'repo3', stargazers_count: 8 }
+        ];
+
+
+    const filteredRepositories = filterRepositoriesWithMoreThanFiveStars(repositories);
+
+
+    expect(filteredRepositories).toEqual([
+        { id: 1, name: 'repo1', stargazers_count: 10 },
+        { id: 3, name: 'repo3', stargazers_count: 8 }
+    ]);
+    });
+
+    test('Return an empty array if no repositories have more than 5 stars', () => {
+
+    const repositories = [
+        { id: 1, name: 'repo1', stargazers_count: 3 },
+        { id: 2, name: 'repo2', stargazers_count: 5 },
+        { id: 3, name: 'repo3', stargazers_count: 2 }
+    ];
+
+
+    const filteredRepositories = filterRepositoriesWithMoreThanFiveStars(repositories);
+
+
+    expect(filteredRepositories).toEqual([]);
+    });
+});
+//////////////////////////////////////////////////////////////////////////////////////////
+
+const { getLastFiveUpdatedRepositories } = require('../src/repositoryUtils');
+describe('getLastFiveUpdatedRepositories', () => {
+    describe('when it receives an array of objects', () => {
+    test('Return the last 5 updated repositories', () => {
+
+        const repositories = [
+            { id: 1, name: 'repo1', updated_at: '2023-04-01T12:00:00Z' },
+            { id: 2, name: 'repo2', updated_at: '2023-04-02T12:00:00Z' },
+            { id: 3, name: 'repo3', updated_at: '2023-04-03T12:00:00Z' },
+            { id: 4, name: 'repo4', updated_at: '2023-04-04T12:00:00Z' },
+            { id: 5, name: 'repo5', updated_at: '2023-04-05T12:00:00Z' },
+            { id: 6, name: 'repo6', updated_at: '2023-04-06T12:00:00Z' }
+        ];
+        const lastFiveUpdatedRepositories = getLastFiveUpdatedRepositories(repositories);
+
+    expect(lastFiveUpdatedRepositories).toEqual([
+        { id: 6, name: 'repo6', updated_at: '2023-04-06T12:00:00Z' },
+        { id: 5, name: 'repo5', updated_at: '2023-04-05T12:00:00Z' },
+        { id: 4, name: 'repo4', updated_at: '2023-04-04T12:00:00Z' },
+        { id: 3, name: 'repo3', updated_at: '2023-04-03T12:00:00Z' },
+        { id: 2, name: 'repo2', updated_at: '2023-04-02T12:00:00Z' }
+        ]);
+    });
+
+    test('Return all repositories if there are less than 5', () => {
+
+            const repositories = [
+            { id: 1, name: 'repo1', updated_at: '2023-04-01T12:00:00Z' },
+            { id: 2, name: 'repo2', updated_at: '2023-04-02T12:00:00Z' },
+            { id: 3, name: 'repo3', updated_at: '2023-04-03T12:00:00Z' }
+            ];
+
+
+        const lastFiveUpdatedRepositories = getLastFiveUpdatedRepositories(repositories);
+
+
+        expect(lastFiveUpdatedRepositories).toEqual(repositories);
+        });
+});
+
+const { sumOfAllRepositoryStars } = require('../src/repositoryUtils');
+
+describe('sumOfAllRepositoryStars', () => {
+    describe('when it receives an array of objects', () => {
+    test('Return the sum of all repository stars', () => {
+
+        const repositories = [
+        { id: 1, name: 'repo1', stargazers_count: 10 },
+        { id: 2, name: 'repo2', stargazers_count: 20 },
+        { id: 3, name: 'repo3', stargazers_count: 30 }
+        ];
+
+
+    const totalStars = sumOfAllRepositoryStars(repositories);
+
+
+    expect(totalStars).toBe(10 + 20 + 30);
+    });
+
+    test('Return 0 if there are no repositories', () => {
+
+    const repositories = [];
+
+
+    const totalStars = sumOfAllRepositoryStars(repositories);
+
+
+    expect(totalStars).toBe(0);
+    });
+});
